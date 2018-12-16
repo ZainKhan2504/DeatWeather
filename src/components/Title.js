3c423c40371d0454105150403e3907f659b792d4
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-12-16 16:31:39 +0500

    Added Custom CSS

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Title = () => (
   <div>
-    <h1>Deat Weather</h1>
-    <p>Find out temperature, conditions and more...</p>
+    <h1 className="title-container__title">Deat Weather</h1>
+    <h3 className="title-container__subtitle">
+      Find out temperature, conditions and more...
+    </h3>
   </div>
 );
 
